8f9821669c65a1f8eaa596e42f5399163deb46af
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2019-11-12 12:03:51 -0500

    Updated motor controls.
    Made buttons move motors by 15 degrees. Also beautified code.

---
+++ GremlinsGimble.ino
@@ -0,0 +1,138 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Servo.h>
+
+int led = 4;
+Servo panServo;
+Servo tiltServo;
+int panPos = 0;
+int tiltPos = 0;
+int
+
+byte mac[] = {
+  0xDE,
+  0xAD,
+  0xBE,
+  0xEF,
+  0xFE,
+  0xED
+}; //physical mac address
+byte ip[] = {
+  192,
+  168,
+  1,
+  178
+}; // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
+byte gateway[] = {
+  192,
+  168,
+  1,
+  1
+}; // internet access via router
+byte subnet[] = {
+  255,
+  255,
+  255,
+  0
+}; //subnet mask
+EthernetServer server(80); //server port
+String readString;
+
+void setup() {
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip, gateway, subnet);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  pinMode(led, OUTPUT);
+  panServo.attach(9);
+  tiltServo.attach(8);
+}
+
+void loop() {
+  // Create a client connection
+  EthernetClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        //read char by char HTTP request
+        if (readString.length() < 100) {
+          //store characters to string
+          readString += c;
+          //Serial.print(c);
+        }
+
+        //if HTTP request has ended
+        if (c == '\n') {
+          Serial.println(readString); //print to serial monitor for debuging
+
+          client.println("HTTP/1.1 200 OK"); //send new page
+          client.println("Content-Type: text/html");
+          client.println();
+          client.println("<HTML>");
+          client.println("<HEAD>");
+          client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
+          client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
+          client.println("<link rel='stylesheet' type='text/css' href='https://github.com/Vasu-Baldwa/ArduinoInternetGimble/blob/master/GremlinsGimble.css' />");
+          client.println("<TITLE>Dixie's Camera</TITLE>");
+          client.println("</HEAD>");
+          client.println("<BODY>");
+          client.println("<H1>Control the Gremlin's Gimble</H1>");
+          client.println("<hr />");
+          client.println("<br />");
+          client.println("<H2>Fall 2019: A Christmas Carol</H2>");
+          client.println("<br />");
+          client.println("<a href=\"/?button1on\"\">Rotate Left</a>");
+          client.println("<a href=\"/?button1off\"\">Rotate Right</a><br />");
+          client.println("<br />");
+          client.println("<br />");
+          client.println("<a href=\"/?button2on\"\">Pan Left</a>");
+          client.println("<a href=\"/?button2off\"\">Pan Right</a><br />");
+          client.println("<br />");
+          client.println("</BODY>");
+          client.println("</HTML>");
+
+          delay(1);
+          //stopping client
+          client.stop();
+		  
+          //controls the Arduino if you press the buttons
+          if (readString.indexOf("?button1on") > 0) {
+            tiltPos += 15
+            tiltServo.write(tiltPos);
+            delay(5);
+          }
+          if (readString.indexOf("?button1off") > 0) {
+            tiltPos -= 15
+            tiltServo.write(tiltPos);
+            delay(5);
+          }
+          if (readString.indexOf("?button2on") > 0) {
+            panPos -= 15
+            panServo.write(panPos);
+            delay(5);
+          }
+          if (readString.indexOf("?button2off") > 0) {
+            panPos -= 15
+            panServo.write(panPos);
+            delay(5);
+          }
+
+          //clearing string for next read
+          readString = "";
+
+        }
+      }
+    }
+  }
+}
